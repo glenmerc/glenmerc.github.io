4843483879ee70e6f3ab9feba290bdc4f1de074f
glenmerc <glenmerc@student.arteveldehs.be>
2016-11-30 13:36:31 +0100

    update

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $( document ).ready(function() {
 	
 	
-	$.getJSON("http://ip-api.com/json", function(data){
+	$.getJSON("https://ip-api.com/json", function(data){
 		var city = data.city;
 		
 		$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=d7b9ba0ae6a5acee57e21df562bd2fe0", function (data) { 

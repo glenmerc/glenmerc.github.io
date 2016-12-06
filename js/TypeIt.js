d90957cdc8a29b4a62b759346f18ebc37adf74a7
glenmerc <glenmerc@student.arteveldehs.be>
2016-12-06 13:51:55 +0100

    index img

--- js/TypeIt.js
+++ js/TypeIt.js
@@ -6,6 +6,8 @@ $( document ).ready(function() {
 		
 		
 		i = Math.floor((Math.random() * 100) + 1);
+	
+	$("body").css({"background-image": "url(../../img/index/"+data.Quotes[i].Img+")"});
 		
 	$(".typing").typeIt({
      strings: ['“'+ data.Quotes[i].Quote +'” <p class="ptit">'+ data.Quotes[i].Film +', '+ data.Quotes[i].Jaar  +'</p>'],

9587373aca67c5fd6f0a9ba12cea460cfc049ce7
glenmerc <glenmerc@student.arteveldehs.be>
2016-12-10 11:28:32 +0100

    integration login

---
+++ js/login.js
@@ -0,0 +1,107 @@
+$( document ).ready(function() {
+	
+	// Initialize Firebase
+	var config = {
+   	 	apiKey: "AIzaSyBQT460DP2_mPN0E_s5Ike9F-XvbGNGVAI",
+    	authDomain: "morelikeit-1477411089338.firebaseapp.com",
+   		databaseURL: "https://morelikeit-1477411089338.firebaseio.com",
+    	storageBucket: "morelikeit-1477411089338.appspot.com",
+   		 messagingSenderId: "848495272237"
+  	};
+	
+  	firebase.initializeApp(config);
+	
+	//verkrijg de values van loginscherm
+	const txtEmail = document.getElementById("txtEmail");
+	const txtPassword = document.getElementById("txtPassword");
+	const btnLogin = document.getElementById("btnLogin");
+	const btnSignUp = document.getElementById("btnSignUp");
+	const btnLogout = document.getElementById("btnLogout");
+	
+	//voeg login event toe
+	btnLogin.addEventListener("click", e =>{
+		//verkrijg de values
+		const email = txtEmail.value;
+		const pass = txtPassword.value;
+		const auth = firebase.auth();
+		//sign-in
+		const promise = auth.signInWithEmailAndPassword(email,pass);
+		promise.catch(e => console.log(e.message));
+		});
+	
+	//voeg sign up event toe
+	btnSignUp.addEventListener("click", e =>{
+		//verkrijg de values
+		const email = txtEmail.value;
+		const pass = txtPassword.value;
+		const auth = firebase.auth();
+		//sign up
+		const promise = auth.createUserWithEmailAndPassword(email,pass);
+		promise.catch(e => console.log(e.message));
+		});
+	
+	//voeg log out event toe
+	btnLogout.addEventListener("click", e =>{
+		//verkrijg de values
+		firebase.auth().signOut();
+		});
+	
+	//realtime listener
+	firebase.auth().onAuthStateChanged(firebaseUser =>{
+		if (firebaseUser){
+			console.log(firebaseUser);
+			$("#btnLogout").removeClass("hide");
+			$("#btnLogin").addClass("hide");
+			$("#btnSignUp").addClass("hide");
+		}else{
+			console.log("not logged in");
+			$("#btnLogout").addClass("hide");
+			$("#btnLogin").removeClass("hide");
+			$("#btnSignUp").removeClass("hide");
+		}
+	});
+	
+	var provider = new firebase.auth.FacebookAuthProvider();
+	
+	$("#btnFacebook").click(function(){
+		firebase.auth().signInWithPopup(provider).then(function(result) {
+ 			// This gives you a Facebook Access Token. You can use it to access the Facebook API.
+  			var token = result.credential.accessToken;
+  			// The signed-in user info.
+  			var user = result.user;
+  			// ...
+		}).catch(function(error) {
+  			// Handle Errors here.
+  			var errorCode = error.code;
+  			var errorMessage = error.message;
+  			// The email of the user's account used.
+  			var email = error.email;
+  			// The firebase.auth.AuthCredential type that was used.
+  			var credential = error.credential;
+  			// ...
+	});
+	
+	var provider1 = new firebase.auth.GoogleAuthProvider();
+	
+	$("#btnGoogle").click(function(){
+		firebase.auth().signInWithPopup(provider1).then(function(result) {
+  			// This gives you a Google Access Token. You can use it to access the Google API.
+  			var token = result.credential.accessToken;
+  			// The signed-in user info.
+  			var user = result.user;
+  			// ...
+		}).catch(function(error) {
+  			// Handle Errors here.
+  			var errorCode = error.code;
+  			var errorMessage = error.message;
+  			// The email of the user's account used.
+  			var email = error.email;
+  			// The firebase.auth.AuthCredential type that was used.
+  			var credential = error.credential;
+  			// ...
+	});
+});
+	
+	
+	});
+});
---
+++ login.md
@@ -0,0 +1,36 @@
+---
+layout  : theatres
+title   : "New Media Design & Development I"
+---
+
+<div class="push">
+<div class="login">
+		<div class="login-screen">
+			<div class="app-title">
+				<h1 class="white">Login</h1>
+			</div>
+
+			<div class="login-form">
+				<div class="control-group">
+					<input  id="txtEmail" type="email" class="login-field" value="" placeholder="Email">
+					<label class="login-field-icon fui-user" for="login-name"></label>
+				</div>
+
+				<div class="control-group">
+					<input id="txtPassword" type="password" class="login-field" value="" placeholder="Password">
+					<label class="login-field-icon fui-lock" for="login-pass"></label>
+				</div>
+				
+				
+					<button id="btnSignUp" class="btn btn-primary btn-large btn-block">Sign up</button>
+					
+					<button id="btnLogin" class="btn btn-primary btn-large btn-block">Login</button>
+					
+					<button id="btnLogout" class="btn btn-primary btn-large btn-block hide">Logout</button>
+					
+					
+					<a class="login-link" href="#">Lost your password?</a>
+			</div>
+		</div>
+</div>
+</div>
